50239e372f6dfd8b1c2b8aad7c7ec020fd134815
SergeyGriko <206880@gmail.com>
2025-05-17 07:59:17 +0300

    6

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,53 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача Первая");
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         System.out.println("Задача Вторая");
-        for (int i = 10; i > 0; i--) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
         System.out.println("Задача Третья");
-        for (int i = 0; i < 17; i += 2) {
+        for (int i = 0; i <= 16; i += 2) {
             System.out.println(i);
         }
         System.out.println("Задача Четвертая");
-        for (int i = 10; i > -11; i--) {
+        for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
         System.out.println("Задача Пятая");
-        for (int i = 1904; i < 2097; i += 4) {
+        for (int i = 1904; i <= 2096; i += 4) {
             System.out.println(i + " год является високосным");
+        }
+        System.out.println("Задача Шестая");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Седьмая");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Восьмая");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Задача Девятая");
+        int salaryProcent = 29000;
+        int totalProcent = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalProcent = totalProcent + totalProcent / 100;
+            totalProcent = totalProcent + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalProcent + " рублей");
+        }
+        System.out.println("Задача Деcятая");
+        int number = 2;
+        for (int i = 1; i <= 10; i++) {
+            int result = number*i;
+            System.out.println(number + " * " + i + " = " + result);
         }
     }
 }
