d2d6a1a577a09d1f21c235af7469933d1a518cf6
SergeyGriko <206880@gmail.com>
2025-05-17 06:42:47 +0300

    5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача Первая");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Вторая");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Третья");
+        for (int i = 0; i < 17; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Четвертая");
+        for (int i = 10; i > -11; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Задача Пятая");
+        for (int i = 1904; i < 2097; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
     }
 }
